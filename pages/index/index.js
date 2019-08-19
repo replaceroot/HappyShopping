bf25d2a7213b69a805c79e8b8d84712feebf2a51
replaceroot <1762458611@qq.com>
2019-08-20 00:09:07 +0800

    首页-头部分类导航完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,31 +1,44 @@
 // pages/index/index.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     // 轮播图数组
-    swiperList:[]
+    swiperList: [],
+    // 分类导航数组
+    navCateList: []
   },
   // onLoad 页面开始加载
-  onLoad(){
+  onLoad() {
     this.getSwiperList();
+    this.getNavCateList();
   },
   // 获取轮播图数据方法
-  getSwiperList(){
+  getSwiperList() {
     /* 发送请求获取数据 */
-    var reqTask = wx.request({
-      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
-      
-      success: (result) => {
-        console.log(result.data.message);
+    wx.request({
+      url: "https://api.zbztb.cn/api/public/v1/home/swiperdata",
+      success: result => {
+        // console.log(result.data.message);
         this.setData({
           swiperList: result.data.message
-        })
-      },
+        });
+      }
     });
-      
-  }
+  },
 
-})
+  // 获取分类导航数据方法
+  getNavCateList() {
+    wx.request({
+      url: "https://api.zbztb.cn/api/public/v1/home/catitems",
+      
+      success: result => {
+        console.log(result.data.message);
+        this.setData({
+          navCateList: result.data.message
+        })
+      }
+    });
+  }
+});
