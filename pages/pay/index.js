a33c4a6525c48b117a712fe1eb55e6f66c20a6fe
replaceroot <1762458611@qq.com>
2019-08-31 10:17:05 +0800

    支付页面-页面的动态渲染

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,66 +1,43 @@
-// pages/pay/index.js
+
+/* 
+  1 动态渲染的商品应该是 checked=true的这些商品
+  
+*/
+
+import regeneratorRuntime from "../../lib/runtime/runtime";
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    address: {},
+    cart: {},
+    totalNum: 0,
+    totalPrice: 0,
+  },
+  // 页面切换显示的时候 触发 onShow
+  onShow() {
+    // 1 获取本地存储中的 收货地址数据 默认值 空字符串
+    const address = wx.getStorageSync("address") || {};
+    // 获取购物车数据
+    const cart = wx.getStorageSync("cart") || {};
+    // 2 把address存入data中
+    this.setData({ address });
+    this.setCart(cart);
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  // 设置购物车数据和设置总价格
+  setCart(cart) {
+    // 0 把购物车对象转成数组
+    let cartArr = Object.values(cart);
 
+    let totalPrice = 0;
+    // 3 计算总数量
+    let totalNum = 0;
+    cartArr.forEach(v => {
+      if (v.checked) {
+        totalPrice += v.num * v.goods_price;
+        totalNum += v.num;
+      }
+    });
+    this.setData({ cart, totalPrice, totalNum });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+});
